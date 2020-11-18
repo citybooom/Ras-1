de46996fc82f07324adfa251af3d8fca51b6d7ab
Calum <briggscalum@gmail.com>
2020-11-18 11:50:17 -0500

    Reduntant

--- Ras/PyQtTest.py
+++
@@ -1,5 +0,0 @@
-#!/bin/env python
-
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
-import sys
--- Ras/boundadd.m
+++
@@ -1,8 +0,0 @@
-function f = boundadd(a,b,lower, upper)
-    if(a + b <= upper)
-        f = a + b;
-    else
-        f = lower + (a+b) - upper - 1;
-    end
-end
-
