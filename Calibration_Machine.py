0c1eca6a0b96fda61faf61bcf137277e14e49369
Calum <cbriggs@raslabs.com>
2020-12-14 12:03:31 -0500

    Auto stash before merge of "master" and "origin/master"

--- Calibration_Machine.py
+++ Calibration_Machine.py
@@ -1,13 +1,13 @@
 import serial
 
 
-ser = serial.Serial('COM19',timeout=5)
+ser = serial.Serial('COM5',timeout=5)
 ser.baudrate = 57600
 
-ser2 = serial.Serial('COM11', timeout=0)
+ser2 = serial.Serial('COM3', timeout=0)
 ser2.baudrate = 115200
 
-f = open("Test_1_C4.txt", "a")
+f = open("Test_400K_C3_normalround_500g.txt", "a")
 
 trial_pos = '0_0'
 count = 0
